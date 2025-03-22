2685d77d50a5ae6287090d93c9574ba143007924
shubhamsinghb <hackathonttt@gmail.com>
2025-03-22 21:29:53 +0530

    updating trst

--- python_example_code/test_run.py
+++ python_example_code/test_run.py
@@ -14,6 +14,7 @@ def run(playwright: Playwright) -> None:
     page.set_default_timeout(40000)
     page.wait_for_load_state()
     page.locator("text='Log In'").click()
+    time.sleep(30)
     # page.click("button:has-text('Log In')", timeout=60000)
     page.get_by_test_id("signUp.switchToSignUp").click()
     page.get_by_test_id("siteMembersDialogLayout").get_by_test_id("buttonElement").click()
@@ -22,12 +23,14 @@ def run(playwright: Playwright) -> None:
     page.get_by_label("Password").fill("123123123")
     page.get_by_test_id("submit").get_by_test_id("buttonElement").click()
     page.get_by_role("button", name="testingit account menu").click()
+    page.get
     assert page.is_visible("text=My Orders")
     page.get_by_role("link", name="My Orders").click()
+    # pa
 
-    # ---------------------
-    context.close()
-    browser.close()
+    # # ---------------------
+    # context.close()
+    # browser.close()
 
 
 with sync_playwright() as playwright:
